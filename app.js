72a997b87ce370961bcd6297a2bc1e03dd723d31
Monticolo Juan Ignacio <juan.monticolo@gmail.com>
2023-09-12 21:26:15 -0300

    v1.2
    Guardado en LocalStorage

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 class Calculadora {
     sumar(num1, num2) {
-        return num1 + num2;
+        return num1 + num2
     }
 
     restar(num1, num2) {
-        return num1 - num2;
+        return num1 - num2
     }
 
     dividir(num1, num2) {
-        return num1 / num2;
+        return num1 / num2
     }
 
     multiplicar(num1, num2) {
-        return num1 * num2;
+        return num1 * num2
     }
 }
 
 class Display {
     constructor(ValorAnterior, ValorActual) {
-        this.ValorActual = ValorActual;
-        this.ValorAnterior = ValorAnterior;
-        this.calculador = new Calculadora();
-        this.tipoOperacion = undefined;
-        this.valorActual = '';
-        this.valorAnterior = '';
+        this.ValorActual = ValorActual
+        this.ValorAnterior = ValorAnterior
+        this.calculador = new Calculadora()
+        this.tipoOperacion = undefined
+        this.valorActual = ''
+        this.valorAnterior = ''
+        this.resultado= ''
         this.signos = {
             sumar: '+',
             dividir: '%',
@@ -33,56 +34,73 @@ class Display {
     }
 
     borrar() {
-        this.valorActual = this.valorActual.toString().slice(0, -1);
-        this.imprimirValores();
+        this.valorActual = this.valorActual.toString().slice(0, -1)
+        this.imprimirValores()
     }
 
     borrarTodo() {
-        this.valorActual = '';
-        this.valorAnterior = '';
-        this.tipoOperacion = undefined;
-        this.imprimirValores();
+        this.valorActual = ''
+        this.valorAnterior = ''
+        this.tipoOperacion = undefined
+        this.imprimirValores()
     }
 
     computar(tipo) {
-        this.tipoOperacion !== 'igual' && this.calcular();
-        this.tipoOperacion = tipo;
-        this.valorAnterior = this.valorActual || this.valorAnterior;
-        this.valorActual = '';
-        this.imprimirValores();
+        this.tipoOperacion !== 'igual' && this.calcular()
+        this.tipoOperacion = tipo
+        this.valorAnterior = this.valorActual || this.valorAnterior
+        this.valorActual = ''
+        this.imprimirValores()
     }
 
     agregarNumero(numero) {
         if (numero === '.' && this.valorActual.includes('.')) return
-        this.valorActual = this.valorActual.toString() + numero.toString();
-        this.imprimirValores();
+        this.valorActual = this.valorActual.toString() + numero.toString()
+        this.imprimirValores()
     }
 
     imprimirValores() {
-        this.ValorActual.textContent = this.valorActual;
-        this.ValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
+        this.ValorActual.textContent = this.valorActual
+        this.ValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`
     }
 
     calcular() {
-        const valorAnterior = parseFloat(this.valorAnterior);
-        const valorActual = parseFloat(this.valorActual);
+        const valorAnterior = parseFloat(this.valorAnterior)
+        const valorActual = parseFloat(this.valorActual)
 
         if (isNaN(valorActual) || isNaN(valorAnterior)) return
-        this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
+        this.resultado = this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual)
+        this.guardarEnStorage()
+    }
+
+    guardarEnStorage(){
+        let resultadojson = JSON.stringify(this.resultado)
+        localStorage.setItem("resultado", resultadojson)
+    }
+
+    recuperarStorage(){
+        let resultadojson = localStorage.getItem("resultado")
+        let resultadojs = JSON.parse(resultadojson)
+        let Aux = ""
+        resultadojs.forEach( resultado => {
+            let nuevodisplay = new Displaay(this.valorAnterior,this.valorActual)
+            listaAux.push(nuevodisplay)
+        })
+        this.resultado = listaAux
     }
 }
 
-const ValorAnterior = document.getElementById('valor-anterior');
-const ValorActual = document.getElementById('valor-actual');
-const botonesNumeros = document.querySelectorAll('.numero');
-const botonesOperadores = document.querySelectorAll('.operador');
+const ValorAnterior = document.getElementById('valor-anterior')
+const ValorActual = document.getElementById('valor-actual')
+const botonesNumeros = document.querySelectorAll('.numero')
+const botonesOperadores = document.querySelectorAll('.operador')
 
-const display = new Display(ValorAnterior, ValorActual);
+const display = new Display(ValorAnterior, ValorActual)
 
 botonesNumeros.forEach(boton => {
-    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
-});
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML))
+})
 
 botonesOperadores.forEach(boton => {
     boton.addEventListener('click', () => display.computar(boton.value))
-});
+})
